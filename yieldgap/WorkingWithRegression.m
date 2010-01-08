8df172e6d52164433db810fdf6558992849d35bc
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-01-08 18:11:48 +0000

    latest
    git-svn-id: file:///Users/svnuser/matlab@30 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldgap/WorkingWithRegression.m
+++ yieldgap/WorkingWithRegression.m
@@ -1,18 +1,25 @@
 % have 
- CultivatedArea      4320x2160            74649600  double              
-  GDD                 4320x2160            74649600  double              
-  TMI                 4320x2160            74649600  double              
-  Yield               4320x2160            74649600  double   
+CultivatedArea      4320x2160            74649600  double              
+GDD                 4320x2160            74649600  double              
+TMI                 4320x2160            74649600  double              
+Yield               4320x2160            74649600  double   
   
   
   
-  iiLogical=(CultivatedArea >0 & isfinite(CultivatedArea) ...
-      & DataMaskLogical & isfinite(Yield) & isfinite(TMI));
+iiLogical=(CultivatedArea >0 & isfinite(CultivatedArea) ...
+  & DataMaskLogical & isfinite(Yield) & isfinite(TMI) ...
+  & Yield<1e10);
   ii=find(iiLogical);
 
+  spare GDD TMI Yield CultivatedArea ii iiLogical
+  DoubleAllVars
   g=GDD(ii);
   t=TMI(ii);
   y=Yield(ii);
   w=CultivatedArea(ii);
+  s = t*.01;
+  s = s(:);
   
-  
+FO = fitoptions('Method', 'LinearLeastSquares')
+x = [g t s];
+[FO, G, O] = FIT(x, y, 'poly111', 'weight', w)

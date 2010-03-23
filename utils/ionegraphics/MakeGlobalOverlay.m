71b90a66b70e9a700af2eed46f85c16bee0ea292
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-03-23 21:26:23 +0000

    introduced MakeGlobalOverlay and scalecolors
    git-svn-id: file:///Users/svnuser/matlab@88 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionegraphics/MakeGlobalOverlay.m
@@ -0,0 +1,87 @@
+function MakeGlobalOverlay(Data,colormap,coloraxis,FullFileName,BaseTransparency);
+% MakeGlobalOverlay - turn a netcdf into a globe-spanning .png 
+%
+%  Syntax
+%  
+%      MakeGlobalOverlay(Data,colormap,coloraxis,FileSaveName,BaseOpacity);
+%
+%  Note:  any values with NaN will be made completely transparent
+% 
+%  Example
+%
+% SystemGlobals
+%   S=OpenNetCDF([IoneDataDir '/Crops2000/crops/maize_5min.nc'])
+% 
+%   Area=S.Data(:,:,1);
+%   Yield=S.Data(:,:,2);
+%   Yield(Yield>1e10)=NaN;
+%  Data=Yield;
+%  cmap=finemap(colormap);
+%  coloraxis=[0 12]
+%  MakeGlobalOverlay(Data,'revsummer',[0 12],'fig2.png',0.5)
+
+
+if nargin==0
+  help(mfilename)
+  return
+end
+
+Data(Data>1e10)=NaN;
+
+
+if nargin<2
+  colormap='summer';
+end
+if nargin<3 | isempty(coloraxis)
+  ii=find(isfinite(Data));
+  tmp01=Data(ii);
+  coloraxis=[min(tmp01) max(tmp01)]
+end
+if nargin<4
+  [filename,pathname]=uiputfile('*.png','Save file name');
+  FullFileName=fullfile(pathname,filename);
+end
+if nargin<5
+  BaseTransparency=0.5;
+end
+
+
+
+
+iiNaN=find(isnan(Data));
+
+Alpha=ones(size(Data))*BaseTransparency;
+
+Alpha(iiNaN)=0;
+
+imagearray=zeros([size(Data) 3]);
+
+cmap=finemap(colormap);
+
+scaled=(Data-coloraxis(1))/(diff(coloraxis));
+scaled(scaled>1)=1;
+scaled(scaled<0)=0;
+
+%loop to scale to colormap
+for j=1:3
+  imagearray(:,:,j)=ScaleColors(scaled,cmap(:,j));
+end
+
+% loop to scale NaNs to white [doesn't really matter: will make
+% them 100% transparent later.  nice to do only for intermediate visualization.]
+for j=1:3
+  tmp=imagearray(:,:,j);
+  tmp(iiNaN)=255;
+  imagearray(:,:,j)=tmp;
+end
+  
+% loop to rotate imagearray
+for j=1:3;
+  newimagearray(:,:,j)=  imagearray(:,:,j).';
+  Alpha=Alpha.';
+end
+imagearray=newimagearray;
+
+imagearray=uint8(imagearray);
+% image(imagearray)
+ imwrite(imagearray,FullFileName,'png','Alpha',uint8(Alpha*255));

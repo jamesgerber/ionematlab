0b0ed5ba8bf737fa453340ed5425c631e308e780
andrew <andrew@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-07-20 19:04:36 +0000

    MakeIndexed added
    git-svn-id: file:///Users/svnuser/matlab@155 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionegraphics/MakeIndexed.m
@@ -0,0 +1,13 @@
+function [IM CMAP]=MakeIndexed(Image)
+%Image is a normal rgb image, outputs an indexed image and its associated
+%colormap. Inefficient, but some functions can only use indexed images.
+CMAP=zeros(size(Image,1)*size(Image,2),3);
+IM=zeros(size(Image,1),size(Image,2));
+CNext=1;
+for i=1:size(Image,1)
+    for j=1:size(Image,2)
+        CMAP(CNext,:)=[Image(i,j,1) Image(i,j,2) Image(i,j,3)];
+        IM(i,j)=CNext;
+        CNext=CNext+1;
+    end
+end

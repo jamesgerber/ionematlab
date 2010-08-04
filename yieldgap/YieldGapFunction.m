a31297f094a67b7ebd00fe3cd9b70fe902d3bf4a
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-08-04 01:31:29 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@174 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldgap/YieldGapFunction.m
+++ yieldgap/YieldGapFunction.m
@@ -55,49 +55,7 @@ end
 %% Determine filename tokens
 
 
-[DS,NS]=CSV_to_structure('crops.csv');
-
-% cropname
-j=FS.CropNo;
-cropname=NS.col1{j};
-cropname=makesafestring(cropname);
-
-% Revision
-switch FS.ClimateSpaceRev
-    case 'K'
-        ClimateSpaceDescription='AreaFilteredClimateSpace';
-    case 'L'
-        ClimateSpaceDescription='AreaFilteredClimateSpaceWithSoil';
-    case 'H'
-        ClimateSpaceDescription='ContourFilteredClimateSpace';
-    case 'M'
-        ClimateSpaceDescription='ContourFilteredClimateSpaceWithSoil';
-    otherwise
-        ClimateSpaceDescription=['ClimateSpaceRev'  FS.ClimateSpaceRev];
-end
-
-
-        %%
-try
-    SystemGlobals
-    switch FS.ClimateSpaceRev
-        case {'F','H','I','J','K','L','M'}
-            FileName=[OutputDirBase '/YieldGap_' cropname '_' ...
-                'MaxYieldPct_' num2str(FS.PercentileForMaxYield) ...
-                '_'  ClimateSpaceDescription ...
-                '_' num2str(FS.ClimateSpaceN) 'x' num2str(FS.ClimateSpaceN) '_' FS.WetFlag '.mat'];
-  
-        case 'G'
-               FileName=[OutputDirBase '/YieldGap_CropNo' int2str(FS.CropNo) ...
-                   'MaxYieldPct_' num2str(FS.PercentileForMaxYield) ...
-                '_ClimateSpaceRev'  FS.ClimateSpaceRev ...
-                '_soilrev' FS.csqirev ...
-                '_' num2str(FS.ClimateSpaceN)  '_' FS.WetFlag '.mat'];
-    end
-    
-catch
-    error(['Prob. defining filename.  need CropNo, ClimateSpaceRev, ClimateSpaceN']);
-end
+FileName=YieldGapFunctionFileNames(FS,OutputDirBase);
 
 ForceRedo=0;
 

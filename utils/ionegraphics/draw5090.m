a9bb1c85cc331be009fb252c7abcd94b126ea9e0
andrew <andrew@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-08-05 19:40:43 +0000

    draw5090 added
    git-svn-id: file:///Users/svnuser/matlab@179 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionegraphics/draw5090.m
@@ -0,0 +1,19 @@
+function draw5090(Data)
+    load worldindexed;
+    Data=fliplr(EasyInterp2(Data,4320,2160));
+    bmap=immap;
+    backdata=rot90(im,3)+1;
+    tmp=sort(nonzeros(Data));
+    newmap=zeros(length(bmap)+1,3);
+    newmap(1,:)=[0.7,0.0,0.7];
+    newmap(length(newmap)+1,:)=[0.9,0.9,0.0];
+    for i=1:length(bmap)
+        newmap(i+1,:)=bmap(i,:);
+    end
+    backdata(Data>tmp(round(length(tmp)*.495))&Data<tmp(round(length(tmp)*.505)))=1;
+    backdata(Data>tmp(round(length(tmp)*.895))&Data<tmp(round(length(tmp)*.905)))=length(newmap);
+    hm=axesm('robinson');
+    NumPointsPerDegree=12;
+    R= [NumPointsPerDegree,90,-180];
+    h=meshm(double(backdata.'),R,[50 100],-1);
+    colormap(newmap);

71579ea992723479f1dc31c9efa39ef66dfb294f
andrew <andrew@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-11-03 19:23:08 +0000

    Personal preferences for nicesurfgeneral!
    git-svn-id: file:///Users/svnuser/matlab@216 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionedata/personalpreferences.m
@@ -0,0 +1,26 @@
+function x=personalpreferences(variable,setting)
+% PERSONALPREFERENCES - get or set personal default settings for
+% NiceSurfGeneral
+%
+%  Syntax
+%
+%      x=personalpreferences(variable) - returns the current setting for
+%      variable, or '' if none.
+%
+%      x=personalpreferences(variable,setting) - sets variable to setting,
+%      returns previous setting
+%
+oceancolor='';
+nodatacolor='';
+latlongcolor='';
+printingres='';
+load personalprefsactual
+try
+    eval(['x=' variable ';']);
+catch
+    x='';
+end
+if (nargin>1)
+    eval([variable '=setting;']);
+end
+save('personalprefsactual','oceancolor','nodatacolor','latlongcolor','printingres');

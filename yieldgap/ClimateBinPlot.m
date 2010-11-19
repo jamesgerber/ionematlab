db38f154102343b67bd019bf9b4594da0b9b8b59
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2010-11-19 15:26:51 +0000

    ClimateBinPlot
    git-svn-id: file:///Users/svnuser/matlab@247 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldgap/ClimateBinPlot.m
+++ yieldgap/ClimateBinPlot.m
@@ -1,4 +1,5 @@
-load ClimateMask_Maize_GDD8_prec_5x5_RevH
+function ClimateBinPlot(BinMatrix)
+%load ClimateMask_Maize_GDD8_prec_5x5_RevH
 
 
 a=BinMatrix;
@@ -32,14 +33,15 @@ bb=ones(20,3);%building block
 bc=ones(20,1);%building column
 
 cmap=[];
-for j=1:5
-  cv=vect{j};
-
-  NewBlock=[cv(1)*bc cv(2)*bc cv(3)*bc];
-  for k=1:5
-  cmap=[cmap ; NewBlock*s(k)];
-  end
-end
+% % for j=1:5
+% %   cv=vect{j};
+% % 
+% %   NewBlock=[cv(1)*bc cv(2)*bc cv(3)*bc];
+% %   for k=1:5
+% %   cmap=[cmap ; NewBlock*s(k)];
+% %   end
+% % end
+[cmap,shortmap]=mapbyhand;
 
 newmap=finemap(cmap);%,'emblue','white');
 
@@ -66,6 +68,35 @@ x=y'
 
 
 
+hax=axes('position',[.025 .2 .3 .3],'tag','tmpaxis');%,
+
+x=1:5;
+y=1:5;
+for j=1:5
+  for k=1:5
+      m=(j-1)*5+k;
+   % cv=vect{j};
+   % color=cv*s(k);
+    xvect=[x(j)-.5 ,x(j)+.5, x(j)+.5, x(j)-.5, x(j)-.5];
+    yvect=[y(k)-.5 ,y(k)-.5, y(k)+.5, y(k)+.5, y(k)-.5];    
+
+    patch(xvect,yvect,shortmap(m,1:3))
+  end
+end
+
+zeroxlim(0,6);
+zeroylim(0,6);
+
+set(gca,'visib','off')
+hx=text(3,0.1,'  GDD  ');
+set(hx,'FontSize',12,'HorizontalAlignment','Center','fontweight','bold');
+hy=text(0.125,3,'  precipitation  ');
+set(hy,'FontSize',12,'HorizontalAlignment','Center','Rotation',90,'fontweight','bold');
+
+
+set(gca,'PlotBoxAspectRatioMode','manual')
+
+
 
 figure
 
@@ -73,20 +104,85 @@ x=1:5;
 y=1:5;
 for j=1:5
   for k=1:5
-    cv=vect{j};
-    color=cv*s(k);
+      m=(j-1)*5+k;
+   % cv=vect{j};
+   % color=cv*s(k);
     xvect=[x(j)-.5 ,x(j)+.5, x(j)+.5, x(j)-.5, x(j)-.5];
     yvect=[y(k)-.5 ,y(k)-.5, y(k)+.5, y(k)+.5, y(k)-.5];    
 
-    patch(xvect,yvect,color)
+    patch(xvect,yvect,shortmap(m,1:3))
   end
 end
 
+
 set(gca,'visib','off')
-hx=text(3,0.125,'GDD');
+hx=text(3,0.125,'  GDD  ');
 set(hx,'FontSize',30,'HorizontalAlignment','Center');
-hy=text(0.125,3,'precipitation');
+hy=text(0.125,3,'  precipitation  ');
 set(hy,'FontSize',30,'HorizontalAlignment','Center','Rotation',90);
 
 
 OutputFig('Force','ClimateBinLegend_5x5','-r75')
+
+function [cmap,shortmap]=mapbyhand;
+
+% these colors taken from colorbrewer2
+% blue
+blue=[
+    158 202 225
+    107 174 214
+    66 146 198
+    33 113 181
+    8 69 148];
+
+green=[
+    161 217 155
+    116 196 118
+    65 171 93
+    35 139 69
+    0 90 50];
+
+purple=[
+    188 189 220
+    158 154 200
+    128 125 186
+    106 81 163
+    74 20 134];
+
+orange=[
+    253 174 107
+    253 141 60
+    241 105 19
+    217 72 1
+    140 45 4]
+
+red=[
+    252 146 114
+    251 106 74
+    239 59 44
+    203 24 29
+    153 0 13];
+
+map=[blue;green;purple;orange;red]/255;
+    
+
+
+newmap=[];
+
+for j=1:length(map)
+    for k=1:16
+        newmap(end+1,1:3)=map(j,1:3);
+        %   newmap(end+1:end+16,2)=map(j,2);
+        %   newmap(end+1:end+16,3)=map(j,3);
+    end
+end
+shortmap=map;
+cmap=newmap;
+
+
+    
+
+
+
+
+

ca31459a5f8c5b20272e0575b0fa018cfe076308
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-01-24 22:24:06 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@274 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldgap/YieldGapToNetCDF.m
+++ yieldgap/YieldGapToNetCDF.m
@@ -7,19 +7,22 @@ for j=1:length(a);
     clear DAS
     
     DAS.CodeRevision=[ num2str(OS.RevData.CodeRevisionNo)];
-    DAS.ProcessingData=[ OS.RevData.ProcessingDate];
-
+    DAS.ProcessingDate=[ OS.RevData.ProcessingDate];
+    DAS.FullFileNameKey=FileNameBase;
+    
+    ShortFileBase=makesafestring(['YieldGap_' OS.cropname]);
+    
     DAS.Description='Climate Bins';
-    WriteNetCDF(single(OS.ClimateMask),'BinMatrix',[FileNameBase '_BinMatrix.nc'],DAS);
+    WriteNetCDF(single(OS.ClimateMask),'BinMatrix',[ShortFileBase '_BinMatrix.nc'],DAS);
     
     DAS.Units='tons/ha';
     DAS.Description='Yield Potential';
-    WriteNetCDF(single(OS.ClimateMask),'YieldPotential',[FileNameBase '_YieldPotential.nc'],DAS);
+    WriteNetCDF(single(OS.ClimateMask),'YieldPotential',[ShortFileBase '_YieldPotential.nc'],DAS);
 
     MissingYield=OS.potentialyield-OS.Yield;
     MissingYield(MissingYield<0)=0;
     
     DAS.Description='Yield Gap'
-    WriteNetCDF(single(OS.ClimateMask),'YieldGap',[FileNameBase '_YieldGap.nc'],DAS);
+    WriteNetCDF(single(OS.ClimateMask),'YieldGap',[ShortFileBase '_YieldGap.nc'],DAS);
         
 end

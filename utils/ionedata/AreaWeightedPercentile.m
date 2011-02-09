2f8ab041712758d702e18dec2d5b67499436b3f4
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-02-09 18:59:10 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@290 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/ionedata/AreaWeightedPercentile.m
+++ utils/ionedata/AreaWeightedPercentile.m
@@ -2,6 +2,12 @@ function x=AreaWeightedPercentile(area,data,p);
 %AreaWeightedPercentile - calculate a percentile value by area
 %
 
+if ~isequal(length(area),length(data))
+    error('unequal vector lengths');
+end
+
+    
+    
 [dum,ii]=sort(data);
 
 sortedarea=area(ii);

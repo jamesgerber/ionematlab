d3375f905612e2f30ecf1f7cbf3f08497097b568
muell512 <muell512@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-02-23 21:15:51 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@300 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ yieldmodel/analyzeyieldlim.m
@@ -0,0 +1,269 @@
+function [yieldlim, dN, dNquality, dP, dPquality, dK, dKquality, ...
+    dI, dIquality] = analyzeyieldlim(cropname, modelnumber, ...
+    desiredyield, pylimitdef, yieldmap, datamask, climatemask, nfert, ...
+    pfert, kfert, avgpercirr)
+
+% [yieldlim, dN, dNquality, dP, dPquality, dK, dKquality, ...
+%     dI, dIquality] = analyzeyieldlim(cropname, modelnumber, ...
+%     desiredyield, pylimitdef, yieldmap, datamask, climatemask, nfert, ...
+%     pfert, kfert, avgpercirr)
+%
+% NOTES ON VARIABLES:
+%
+% desiredyield can come in two forms:
+%  1) It can be the percent change in yield to analyze (e.g. 10, 20, 50).
+%     In other words, do you want to look at what limits yield increasing
+%     10%, 20%, etc?
+%  2) It can be a map of the yield you would like to obtain.
+%
+% pylimitdef = defines "limited by potential" as yields > (pylimitdef *
+%     potential yield). In other words, do you count being at 90%, 100%,
+%     80% of potential yield being maxed out? NOTE: need to update this to
+%     USE A MATRIX OF 90th PERCENTILE POTENTIALS
+%
+% 
+
+
+
+
+% manipulate pylimitdef to become a multiplier
+pylimitdef = pylimitdef ./ 100;
+
+% create desired yield map if necessary
+if length(desiredyield) == 1
+    desiredyieldstr = num2str(desiredyield);
+    desiredyield = 1 + (desiredyield./100);
+    desiredyield = yieldmap .* desiredyield;
+    disp(['analyzing management factors limiting a ' desiredyieldstr ...
+        '% increase in ' cropname ' yield']);
+else
+    disp(['analyzing management factors limiting the change in ' ...
+        cropname ' yield specified by the "desiredyield" matrix'])
+end
+
+% set model choice and load (VL ELM OR VL MB)
+switch modelnumber
+    case 1
+        modelname = 'VL_ELM';
+    case 2
+        modelname = 'VL_MB';
+end
+filestr = ['/Users/muell512/Documents/MATLAB/ndm005_mgmtregression/' ...
+    'modeloutput/' cropname '_m3yieldmodeldata_' modelname '.csv'];
+MS = ReadGenericCSV(filestr);
+
+% initialize yieldlim map output
+yieldlim = nan(4320,2160);
+dN = nan(4320,2160);
+dNquality = nan(4320,2160);
+dP = nan(4320,2160);
+dPquality = nan(4320,2160);
+dK = nan(4320,2160);
+dKquality = nan(4320,2160);
+dI = nan(4320,2160);
+dIquality = nan(4320,2160);
+
+% cycle through the bins and calculate modeled yields
+for bin = 1:100
+    
+    if bin == 1
+        disp(['working on ' cropname ' bins 1-24'])
+    elseif bin == 25
+        disp(['working on ' cropname ' bins 25-49'])
+    elseif bin == 50
+        disp(['working on ' cropname ' bins 50-74'])
+    elseif bin == 75
+        disp(['working on ' cropname ' bins 75-100'])
+    end
+    
+    ii = find((datamask == 1) & (climatemask == bin) ...
+        & isfinite(nfert) & isfinite(pfert) & isfinite(kfert));
+    
+    % make double & select bin area
+    yield_bin = double(yieldmap(ii));
+    irr_bin = double(avgpercirr(ii));
+    nfert_bin = double(nfert(ii));
+    pfert_bin = double(pfert(ii));
+    kfert_bin = double(kfert(ii));
+    desiredyield_bin = double(desiredyield(ii));
+    
+    % identify potential and min yields in the bin
+    potyieldbin = MS.potential_yield(bin);
+    minyieldbin = MS.minimum_yield(bin);
+    
+    % initialize the lim_bin codes and nutrient plus vectors
+    lim_bin = zeros(length(yield_bin),1);
+    dN_bin = zeros(length(yield_bin),1);
+    dNQ_bin = zeros(length(yield_bin),1);
+    dP_bin = zeros(length(yield_bin),1);
+    dPQ_bin = zeros(length(yield_bin),1);
+    dK_bin = zeros(length(yield_bin),1);
+    dKQ_bin = zeros(length(yield_bin),1);
+    dI_bin = zeros(length(yield_bin),1);
+    dIQ_bin = zeros(length(yield_bin),1);
+    
+    %     nfertplus_bin = [];
+    %     kfertplus_bin = [];
+    %     pfertplus_bin = [];
+    %     irrplus_bin = [];
+    
+    % get b_nut (aka alpha for MB model)
+    bnut = MS.b_nut(bin);
+    if modelnumber == 2
+        alpha = bnut;
+    end
+    
+    % grab the explanatory variable list
+    cb = num2str(MS.explanatory_variable_list{bin});
+    % add missing zeros if necessary
+    if length(cb) < 4
+        tmp = 4-length(cb);
+        if tmp == 1;
+            cb = ['0' cb];
+        elseif tmp == 2;
+            cb = ['00' cb];
+        end
+    end
+    
+    %%% cycle through cb, look at each individual functional form
+    
+    % examine for nitrogen limitation
+    if str2num(cb(1)) == 1
+        bFitw = str2num(MS.c_N{bin});
+        bininfo = 1;
+        dNQ_bin = ones(length(dNQ_bin),1);
+    else
+        bFitw = str2num(MS.c_N{101});
+        bininfo = 0;
+        dNQ_bin = ones(length(dNQ_bin),1)+1;
+    end
+    switch modelnumber
+        case 1 % VL ELM
+            nfertplus_bin = (log((potyieldbin ./ ...
+                desiredyield_bin) - 1) - bnut) ./ - bFitw;
+        case 2 % VL MB
+            nfertplus_bin = log((1 - (desiredyield_bin ./ ...
+                potyieldbin)) ./ bnut) ./ -bFitw;
+    end
+    dN_bin = nfertplus_bin - nfert_bin;
+    jj = find(imag(dN_bin)~=0);
+    dN_bin(jj) = NaN;
+    dNQ_bin(jj) = 4;
+    if bininfo == 1
+        jj = find(nfertplus_bin > nfert_bin);
+        lim_bin(jj) = 1;
+    end
+    
+    % examine for phosphorus limitation
+    if str2num(cb(2)) == 1
+        bFitw = str2num(MS.c_P2O5{bin});
+        bininfo = 1;
+        dPQ_bin = ones(length(dPQ_bin),1);
+    else
+        bFitw = str2num(MS.c_P2O5{101});
+        bininfo = 0;
+        dPQ_bin = ones(length(dPQ_bin),1)+1;
+    end
+    switch modelnumber
+        case 1 % VL ELM
+            pfertplus_bin = (log((potyieldbin ./ ...
+                desiredyield_bin) - 1) - bnut) ./ - bFitw;
+        case 2 % VL MB
+            pfertplus_bin = log((1 - (desiredyield_bin ./ ...
+                potyieldbin)) ./ bnut) ./ -bFitw;
+    end
+    dP_bin = pfertplus_bin - pfert_bin;
+    jj = find(imag(dP_bin)~=0);
+    dP_bin(jj) = NaN;
+    dPQ_bin(jj) = 4;
+    if bininfo == 1
+        jj = find(pfertplus_bin > pfert_bin);
+        lim_bin(jj) = 1;
+    end
+    
+    % examine for potash limitation
+    if str2num(cb(3)) == 1
+        bFitw = str2num(MS.c_K2O{bin});
+        bininfo = 1;
+        dKQ_bin = ones(length(dKQ_bin),1);
+    else
+        bFitw = str2num(MS.c_K2O{101});
+        bininfo = 0;
+        dKQ_bin = ones(length(dKQ_bin),1)+1;
+    end
+    switch modelnumber
+        case 1 % VL ELM
+            kfertplus_bin = (log((potyieldbin ./ ...
+                desiredyield_bin) - 1) - bnut) ./ - bFitw;
+        case 2 % VL MB
+            kfertplus_bin = log((1 - (desiredyield_bin ./ ...
+                potyieldbin)) ./ bnut) ./ -bFitw;
+    end
+    dK_bin = kfertplus_bin - kfert_bin;
+    jj = find(imag(dK_bin)~=0);
+    dK_bin(jj) = NaN;
+    dKQ_bin(jj) = 4;
+    if bininfo == 1
+        jj = find(kfertplus_bin > kfert_bin);
+        lim_bin(jj) = 1;
+    end
+    
+    
+    
+    %     % examine for phosphorus limitation
+    %     if str2num(cb(2)) == 1
+    %         bFitw = str2num(MS.c_P2O5{bin});
+    %         switch modelnumber
+    %             case 1 % VL ELM
+    %                 pfertplus_bin = (log((potyieldbin ./ ...
+    %                     desiredyield_bin) - 1) - bnut) ./ - bFitw;
+    %             case 2 % VL MB
+    %                 pfertplus_bin = log((1 - (desiredyield_bin ./ ...
+    %                     potyieldbin)) ./ bnut) ./ -bFitw;
+    %         end
+    %         jj = find(pfertplus_bin > pfert_bin);
+    %         lim_bin(jj) = 1;
+    %     end
+    
+    % examine for water limitation
+    if str2num(cb(4)) == 1
+        bFitw(1) = str2num(MS.b_irr{bin});
+        bFitw(2) = str2num(MS.c_irr{bin});
+        switch modelnumber
+            case 1 % VL ELM
+                irrplus_bin = (log((potyieldbin ./ ...
+                    desiredyield_bin) - 1) - bFitw(1)) ./ - bFitw(2);
+            case 2 % VL MB
+                irrplus_bin = log((1 - (desiredyield_bin ./ ...
+                    potyieldbin)) ./ bFitw(1)) ./ -bFitw(2);
+        end
+        % record water and nutrient limited areas
+        jj = find((irrplus_bin > irr_bin) & (lim_bin == 1));
+        lim_bin(jj) = 2;
+        % record just water limited areas
+        jj = find((irrplus_bin > irr_bin) & (lim_bin == 0));
+        lim_bin(jj) = 3;
+        % record change in % irrigated areas
+        dI_bin = irrplus_bin - irr_bin;
+        jj = find(imag(dI_bin)~=0);
+        dI_bin(jj) = NaN;
+        dIQ_bin = ones(1,length(dIQ_bin));
+        dIQ_bin(jj) = 4;
+    else
+        dI_bin = zeros(length(dI_bin),1);
+        dIQ_bin = ones(length(dIQ_bin),1)+2;
+    end
+    
+    % place lim_bin into yieldlim map
+    jj = find(desiredyield_bin > (pylimitdef .* potyieldbin));
+    lim_bin(jj) = 4;
+    yieldlim(ii) = lim_bin;
+    dN(ii) = dN_bin;
+    dNquality(ii) = dNQ_bin;
+    dP(ii) = dP_bin;
+    dPquality(ii) = dPQ_bin;
+    dK(ii) = dK_bin;
+    dKquality(ii) = dKQ_bin;
+    dI(ii) = dI_bin;
+    dIquality(ii) = dIQ_bin;
+end
---
+++ yieldmodel/m3yieldmodel_UI.m
@@ -0,0 +1,356 @@
+function [modyield] = m3yieldmodel_UI(cropname, datamask, ...
+    climatemask, nfert, pfert, kfert, avgpercirr, modelnumber)
+
+% function [modyield] = m3yieldmodel_UI(cropname, datamask, ...
+%     climatemask, nfert, pfert, kfert, avgpercirr, modelnumber)
+%
+% m3yieldmodel_UI will return a modeled yield map based on crop and
+% management characteristics.
+%
+% NOTE: still accesses preliminary results
+%
+%
+
+% set model choice: VL ELM OR VL MB
+switch modelnumber
+    case 1
+        modelname = 'VL_ELM';
+    case 2
+        modelname = 'VL_MB';
+end
+
+filestr = ['/Users/muell512/Documents/MATLAB/ndm005_mgmtregression/' ...
+    'modeloutput/' cropname '_m3yieldmodeldata_' modelname '.csv'];
+MS = ReadGenericCSV(filestr);
+
+% initialize modyield output
+modyield = nan(4320,2160);
+
+% cycle through the bins and calculate modeled yields
+for bin = 1:100
+    
+    if bin == 1
+        disp(['working on ' cropname ' bins 1-24'])
+    elseif bin == 25
+        disp(['working on ' cropname ' bins 25-49'])
+    elseif bin == 50
+        disp(['working on ' cropname ' bins 50-74'])
+    elseif bin == 75
+        disp(['working on ' cropname ' bins 75-100'])
+    end
+    
+    ii = find((datamask == 1) & (climatemask == bin) ...
+        & isfinite(nfert) & isfinite(pfert) & isfinite(kfert));
+    
+    % make double & select bin area
+    irr_bin = double(avgpercirr(ii));
+    nfert_bin = double(nfert(ii));
+    pfert_bin = double(pfert(ii));
+    kfert_bin = double(kfert(ii));
+    
+    % identify potential and min yields in the bin
+    potyieldbin = MS.potential_yield(bin);
+    minyieldbin = MS.minimum_yield(bin);
+    
+    % get b_nut (aka alpha for MB model)
+    bnut = MS.b_nut(bin);
+    if modelnumber == 2
+        alpha = bnut;
+    end
+    
+    % grab the explanatory variable list
+    cb = num2str(MS.explanatory_variable_list{bin});
+    % add missing zeros if necessary
+    if length(cb) < 4
+        tmp = 4-length(cb);
+        if tmp == 1;
+            cb = ['0' cb];
+        elseif tmp == 2;
+            cb = ['00' cb];
+        end
+    end
+    
+    % create "bFitw" - a list of the correct parameters for the model
+    bFitw = [];
+    tmp = MS.c_N{bin};
+    if ~isempty(tmp)
+        tmp = str2num(tmp);
+        bFitw = [bFitw tmp];
+    end
+    tmp = MS.c_P2O5{bin};
+    if ~isempty(tmp)
+        tmp = str2num(tmp);
+        bFitw = [bFitw tmp];
+    end
+    tmp = MS.c_K2O{bin};
+    if ~isempty(tmp)
+        tmp = str2num(tmp);
+        bFitw = [bFitw tmp];
+    end
+    tmp = MS.b_irr{bin};
+    if ~isempty(tmp)
+        tmp = str2num(tmp);
+        bFitw = [bFitw tmp];
+    end
+    tmp = MS.c_irr{bin};
+    if ~isempty(tmp)
+        tmp = str2num(tmp);
+        bFitw = [bFitw tmp];
+    end
+    
+    % first find the number of nutrient variables
+    numnutvars = 0;
+    for m = 1:3
+        if str2num(cb(m)) == 1
+            numnutvars = numnutvars + 1;
+        end
+    end
+    
+    % then find whether irrigation is on or off
+    if str2num(cb(4)) == 1
+        i = 1;
+    elseif str2num(cb(4)) == 0
+        i = 0;
+    end
+    
+    % now create the right combination of explanatory variables for x
+    xtot = [nfert_bin pfert_bin kfert_bin irr_bin];
+    x = [];
+    for m = 1:4
+        if str2num(cb(m)) == 1
+            x = [x xtot(:,m)];
+        end
+    end
+    
+    % now calculate modY according to the appropriate model (specified
+    % by "numnutvars" and "i"
+    
+    switch numnutvars
+        
+        case 0
+            
+            if i == 0
+                
+                % 0 nutrients and no irr: use weighted mean
+                
+                tmp = ones(length(yield_bin),1);
+                modY = wavg_yield_bin.*tmp;
+                cropOS.modyieldmap(ii) = modY;
+                
+            elseif i == 1
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 0 nutrients + irr
+                        
+                        modY = potyieldbin ...
+                            ./ (1 + exp(bFitw(1) - abs(bFitw(2)) ...
+                            .* x(:,1)));
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 0 nutrients + irr
+                        
+                        modY = potyieldbin ...
+                            .* (1 - (abs(bFitw(1)) .* ...
+                            exp(-abs(bFitw(2)) .* x(:,1))));
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+            end
+            
+        case 1
+            
+            if i == 0
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 1 nutrient, no irr
+                        
+                        modY = potyieldbin ...
+                            ./ (1 + exp(bnut - abs(bFitw(1)) ...
+                            .* x(:,1)));
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 1 nutrient, no irr
+                        
+                        modY = potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(1)) .* x(:,1))));
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+                
+            elseif i == 1
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 1 nutrient + irr
+                        
+                        modY = min([(potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(1)) ...
+                            .* x(:,1)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bFitw(2) - abs(bFitw(3)) ...
+                            .* x(:,2))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 1 nutrient + irr
+                        
+                        modY = min([(potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(1)) .* x(:,1))))), ...
+                            (potyieldbin ...
+                            .* (1 - (abs(bFitw(2)) .* ...
+                            exp(-abs(bFitw(3)) .*x(:,2)))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+            end
+            
+        case 2
+            
+            if i == 0
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 2 nutrients, no irr
+                        
+                        modY = min([(potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(1)) ...
+                            .* x(:,1)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(2)) ...
+                            .* x(:,2))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 2 nutrients, no irr
+                        
+                        modY = min([(potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(1)) .* x(:,1))))), ...
+                            (potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(2)) .*x(:,2)))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+                
+            elseif i == 1
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 2 nutrients + irr
+                        
+                        modY = min([(potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(1)) ...
+                            .* x(:,1)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(2)) ...
+                            .* x(:,2)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bFitw(3) - abs(bFitw(4)) ...
+                            .* x(:,3))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 2 nutrients + irr
+                        
+                        modY = min([(potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(1)) .* x(:,1))))), ...
+                            (potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(2)) .*x(:,2))))), ...
+                            (potyieldbin ...
+                            .* (1 - (abs(bFitw(3) .* ...
+                            exp(-abs(bFitw(4)).*x(:,3))))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+            end
+            
+        case 3
+            
+            if i == 0
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 3 nutrients, no irr
+                        
+                        modY = min([(potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(1)) ...
+                            .* x(:,1)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(2)) ...
+                            .* x(:,2)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(3)) ...
+                            .* x(:,3))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 3 nutrients, no irr
+                        
+                        modY = min([(potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(1)) .* x(:,1))))), ...
+                            (potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(2)) .*x(:,2))))), ...
+                            (potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(3)) .*x(:,3)))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+                
+            elseif i == 1
+                
+                switch modelnumber
+                    
+                    case 1 % VL ELM: 3 nutrients + irr
+                        
+                        modY = min([(potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(1)) ...
+                            .* x(:,1)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(2)) ...
+                            .* x(:,2)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bnut - abs(bFitw(3)) ...
+                            .* x(:,3)))), ...
+                            (potyieldbin ...
+                            ./(1 + exp(bFitw(4) - abs(bFitw(5)) ...
+                            .* x(:,4))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                    case 2 % VL MB: 2 nutrients + irr
+                        
+                        modY = min([(potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(1)) .* x(:,1))))), ...
+                            (potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(2)) .*x(:,2))))), ...
+                            (potyieldbin ...
+                            .* (1 - (alpha .* ...
+                            exp(-abs(bFitw(3)) .*x(:,3))))), ...
+                            (potyieldbin ...
+                            .* (1 - (abs(bFitw(4) .* ...
+                            exp(-abs(bFitw(5)).*x(:,4))))))],[],2);
+                        cropOS.modyieldmap(ii) = modY;
+                        
+                end
+            end
+    end
+    
+    if min(modY) < (minyieldbin - .2)
+        warning(['min(modY) ' num2str(min(modY)) ' < minyieldbin ' ...
+            num2str(minyieldbin) ', ' cropname ' bin ' num2str(bin) ...
+            ', cb = ' cb]);
+    end
+    
+    modyield(ii) = modY;
+end
+
+
+
+

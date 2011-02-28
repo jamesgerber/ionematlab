a874962fda2b422bb77c269a226bd6f0c5fe7bb9
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-02-28 22:20:02 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@305 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/ionefile/CropList.m
+++ utils/ionefile/CropList.m
@@ -1,18 +1,53 @@
-function CropList=CropList(DirString,Year);
+function CropList=CropList(DirString,flag);
 % CropList - return a list of crops
 %
-%  
+%
 
 if nargin  ==0
     DirString='';
 end
 
-if nargin ==2
-    error('only have 2000')
+if nargin <2
+    flag='limited';
 end
 
 
+
 a=dir([iddstring '/Crops2000/crops/*' DirString '*_5min.nc']);
+
+
+switch flag
+    case 'limited'
+        
+        b=dir([iddstring '/Crops2000/crops/*' 'RF' '*_5min.nc']);
+        c=dir([iddstring '/Crops2000/crops/*' 'ncome' '*_5min.nc']);
+        
+        for j=1:length(a);
+            anames{j}=a(j).name;
+        end
+        
+        % remove the RF and income-lmited crops
+        removelist=[];
+        for j=1:length(b)
+            thisname=b(j).name;
+            ii=strmatch(thisname,anames,'exact');
+            removelist=[removelist ii];
+        end
+        for j=1:length(c)
+            thisname=c(j).name;
+            ii=strmatch(thisname,anames,'exact');
+            removelist=[removelist ii];
+        end
+        
+        keeplist=setdiff(1:length(a), removelist);
+        
+        a=a(keeplist);
+    case 'all'
+        disp(['warning ... keeping crops such as rainfedmaize']);
+    otherwise
+        error(['don''t know this syntax']);
+end
+
 
 for j=1:length(a)
     CropList{j}=strrep(a(j).name,'_5min.nc','');

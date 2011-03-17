fc46f568b02c38f8910407d44a6b4375e6690dcf
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-03-17 15:57:52 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@323 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionedata/disaggregate_quantity.m
@@ -0,0 +1,17 @@
+function highresdata=disaggregate_quantity(loresdata,N);
+% disaggregate_quantity - spread data out to a smaller resolution
+%
+%  Syntax
+%     highresdata=disaggregate_quantity(loresdata,N);
+%
+%  Example
+%
+%    See also: disaggregate_rate
+
+[r,c]=size(loresdata);
+highresdata(r*N,c*N)=0;
+for j=1:N
+    for m=1:N
+        highresdata(j:N:(6*r),m:N:(6*c))=loresdata/(N^2);
+    end
+end

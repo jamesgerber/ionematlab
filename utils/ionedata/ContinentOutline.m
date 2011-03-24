88b04f8aa20b95e965bcac9ee235e358dd5b45a4
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-03-24 20:26:44 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@332 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/ionedata/ContinentOutline.m
+++ utils/ionedata/ContinentOutline.m
@@ -1,6 +1,41 @@
-function outline=ContinentOutline(ContinentName)
-%     ''    'Africa'    'Americas'    'Antartica'    'Asia'
-%     'Europe'    'Oceania'
+function Outline=ContinentOutline(ContinentName)
+%  ContinentOutline - return outline of continents
+%
+%  SYNTAX
+%     Outline=ContinentOutline(ContinentName)
+%    where ContinentName can be one of the following:
+%
+%
+%     'Africa'
+%    'Americas'
+%    'Antartica'
+%    'Asia'
+%    'Europe'
+%    'Oceania'
+%
+%    'Antartica'
+%    'Australia and New Zealand'
+%    'Caribbean'
+%    'Central America'
+%    'Central Asia'
+%    'Eastern Africa'
+%    'Eastern Asia'
+%    'Eastern Europe'
+%    'Melanesia'
+%    'Micronesia'
+%    'Middle Africa'
+%    'Northern Africa'
+%    'Northern America'
+%    'Northern Europe'
+%    'Polynesia'
+%    'South America'
+%    'South-Eastern Asia'
+%    'Southern Africa'
+%    'Southern Asia'
+%    'Southern Europe'
+%    'Western Africa'
+%    'Western Asia'
+%    'Western Europe'
 
 %ContinentName='Oceania';
 
@@ -10,32 +45,41 @@ load([iddstring '/misc/ContinentOutlineData.mat'])
 ii=strmatch(ContinentName,UNREGION2,'exact');
 
 if isempty(ii)
-  disp(['Don''t know ' Continent ])
-  disp(['try '])
-  unique(UNREGION2);
+    
+    try
+        ii=strmatch(ContinentName,UNREGION1,'exact');
+    catch
+        
+    disp(['Don''t know ' Continent ])
+    disp(['try '])
+    unique(UNREGION2);
+    error
+    end
 end
 
 Outline=DataBlank;
 
 for j=1:length(ii)
-  j
-  FAO=NAME_FAO(ii(j));
-  S3=standardcountrynames(FAO,'NAME_FAO','sage3');
-  Outline=Outline | CountryCodetoOutline(S3{1});
+    j
+    FAO=NAME_FAO(ii(j));
+    if isempty(FAO{1})
+        disp(['ignoring ' NAME_ISO(ii(j))]);
+    else
+        S3=standardcountrynames(FAO,'NAME_FAO','sage3');
+        Outline=Outline | CountryCodetoOutline(S3{1});
+    end
 end
-
-
-
 
 return
 
 
+%% here is code used to generate ContinentOutlineData
 load gadm1_lev0
 for j=1:248;
-  UNREGION1{j}=S(j).UNREGION1;
-  UNREGION2{j}=S(j).UNREGION2;
-  NAME_ISO{j}=S(j).NAME_ISO;
-  NAME_FAO{j}=S(j).NAME_FAO;
+    UNREGION1{j}=S(j).UNREGION1;
+    UNREGION2{j}=S(j).UNREGION2;
+    NAME_ISO{j}=S(j).NAME_ISO;
+    NAME_FAO{j}=S(j).NAME_FAO;
 end
 
 save ContinentOutlineData NAME_FAO NAME_ISO UNREGION1 UNREGION2

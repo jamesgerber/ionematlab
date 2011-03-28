a3a6e92030215d90b15f9f92fdbe277c0205e5d8
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-03-28 21:46:22 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@349 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/ionegraphics/AddColorbarFinalPlus.m
+++ utils/ionegraphics/AddColorbarFinalPlus.m
@@ -1,17 +1,17 @@
 function AddColorbarFinalPlus(Handle);
 % AddColorbarFinalPlus - add a "+"  to final number on colorbar
 if nargin==0;
-  Handle=gcf;
+    Handle=gcf;
 end
 
 if fix(Handle)==Handle
-  % Handle is an integer ... must be figure handle.  Assume it is an
-  % IonEFigure and look for colorbar accordingly.
-  fud=get(Handle,'UserData');
-  cbh=fud.ColorbarHandle;
+    % Handle is an integer ... must be figure handle.  Assume it is an
+    % IonEFigure and look for colorbar accordingly.
+    fud=get(Handle,'UserData');
+    cbh=fud.ColorbarHandle;
 else
- % user passed in a non-integer handle. Assume it is a handle to a colorbar.
-  cbh=Handle;
+    % user passed in a non-integer handle. Assume it is a handle to a colorbar.
+    cbh=Handle;
 end
 
 
@@ -19,14 +19,15 @@ xtl=get(cbh,'XTickLabel');
 N=size(xtl,1);
 C=size(xtl,2);
 for j=1:N;
-  xtlcell{j}=xtl(j,:);
+    xtlcell{j}=xtl(j,:);
 end
 
 for j=length(xtlcell);  %only this line changed from AddColorbarPercent
     tmp=xtlcell{j};
     tmp=strrep(tmp,' ','');
-  %  xtlcell{j}=[ tmp '(+)'];
-      xtlcell{j}=[ '>' tmp ];
+    xtlcell{j}=[ tmp '(+)'];
+    %  xtlcell{j}=[ '\geq ' tmp ];  %doesn't work ... no tex interpreter
+    %  for colorbar
 end
 
 %set(cbh,'XTickLabel',xtlcell);
@@ -38,4 +39,4 @@ set(cbh,'XTickMode','manual')
 
 
 
-  
+
---
+++ utils/ionegraphics/NSG.m
@@ -0,0 +1,9 @@
+function OS=NSG(varargin);
+% NICESURFGENERAL / NSG
+%
+%  call NiceSurfGeneral
+
+
+OS=NiceSurfGeneral(varargin{1:end});
+
+

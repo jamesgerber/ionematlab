9b2dc979ad9b467aae7beeb0471f03f839c51eb1
muell512 <muell512@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-04-19 02:01:41 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@366 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldmodel/analyzeyieldlim2.m
+++ yieldmodel/analyzeyieldlim2.m
@@ -1,25 +1,73 @@
 function [yieldlim, dN, dNquality, dP, dPquality, dK, dKquality, ...
-    dI, dIquality] = analyzeyieldlim(cropname, modelnumber, ...
+    dI, dIquality] = analyzeyieldlim2(cropname, modelnumber, ...
     desiredyield, potentialyield, yieldmap, datamask, climatemask, ...
-    nfert, pfert, kfert, avgpercirr, errorflag, errormap)
+    nfert, pfert, kfert, avgpercirr, errormap)
 
 % [yieldlim, dN, dNquality, dP, dPquality, dK, dKquality, ...
-%     dI, dIquality] = analyzeyieldlim(cropname, modelnumber, ...
+%     dI, dIquality] = analyzeyieldlim2(cropname, modelnumber, ...
 %     desiredyield, potentialyield, yieldmap, datamask, climatemask, ...
-%     nfert, pfert, kfert, avgpercirr, errorflag, errormap)
+%     nfert, pfert, kfert, avgpercirr, errormap)
 %
-% NOTES ON VARIABLES:
+% ANALYZEYIELDLIM2 is a function that analyzes yield-limiting factors and
+% inputs necessary to achieve desired yields based on the M3 yield models.
 %
-% desiredyield can come in two forms:
-%  1) It can be the percent change in yield to analyze (e.g. 10, 20, 50).
-%     In other words, do you want to look at what limits yield increasing
-%     10%, 20%, etc?
-%  2) It can be a map of the yield you would like to obtain.
+% ERROR CORRECTION: this function uses an error correction to "fix"
+% differences between the modeled world and the observed world
 %
-% potentialyield is a map defining what areas are "yield ceiling limited" -
-%     we usually define this as 90th percentile yields within a climate bin
+%                      %%%%%%%%%%%%%%%%%%%%%%
+%                      % NOTES ON VARIABLES %
+%                      %%%%%%%%%%%%%%%%%%%%%%
 %
-% 
+% OUTPUTS
+%
+%   - yieldlim is a categorical map of yield-limiting factors.
+%        - code 1 = nutrient limited
+%        - code 2 = nutrient + water (irrigation) limited
+%        - code 3 = water (irrigation) limited
+%        - code 4 = desired yield (desiredyield) < observed yield
+%                   (yieldmap)
+%        - code 5 = desired yield (desiredyield) > potential yield
+%                   (potentialyield) (note: this could include areas that
+%                   would otherwise be labeled as code 4)
+%   - dNPKI = delta nutrients necessary to achieve the desiredyield. this
+%             can be negative when less nutrients are required to
+%             achieve a desired yield than what is use. also - you cannot
+%             estimate a dNPKI for a yield value above the asymptote (98th
+%             percentile yields).
+%   - dNPKIquality = the quality of the delta nutrient projection
+%        - code 1 = projected change is from a bin-specific coefficient for
+%                   that particular input
+%        - code 2 = projected change is from a global coefficient (slope)
+%                   for that particular input (note: we only revert to
+%                   global slopes for nutrients, for irrigation we just
+%                   don't estimate a response if there is no coefficient -
+%                   this is code 3)
+%        - code 3 = no estimated irrigation response for this bin
+%
+% INPUTS
+%
+%   - cropname: can be any one of the 17 crops modeled (this should be a
+%         lowercase string with no spaces)
+%   - modelnumber: von Liebig logistic model (VL_LM) = 1, von Liebig
+%         Mitscherlich-Baule model (VL_MBM) = 2
+%   - desiredyield = the yield you would like to achieve - this can come in
+%         two forms: 1) it can be the percent change in yield you would
+%         like to analyze (e.g. 10, 20, 50 = a 10%, 20%, 50% increase over
+%         current yields) or 2) it can be a map of the yield you would like
+%         to obtain (i.e. a map of 75th percentile yields)
+%   - potentialyield: a map defining potential yields (such as 90th
+%         percentile yields) - this defines code 5 in the yieldlim map
+%   - datamask: a logical mask defining "good areas" - probably just the
+%         intersection of good data for yields, inputs, and climate (this
+%         is mostly a sanity check ... being careful)
+%   - climatemask: the crop-specific climate bin map
+%   - nfert, pfert, kfert: fertilizer application rates from the M3
+%         fertilizer dataset (in kg/ha)
+%   - avgpercirr: average percent irrigated area over the growing season
+%         from the MIRCA2000 dataset
+%   - errormap: the difference between modeled and observed yields (modeled
+%         minus observed)
+%
 
 
 % create desired yield map if necessary
@@ -33,6 +81,9 @@ else
     disp(['analyzing management factors limiting the change in ' ...
         cropname ' yield specified by the "desiredyield" matrix'])
 end
+
+% calculate the desired yield in the model world based on the errormap
+moddesiredyield = desiredyield + errormap;
 
 % set model choice and load (VL LM OR VL MBM)
 switch modelnumber
@@ -74,34 +125,29 @@ for bin = 1:100
     
     % make double & select bin area
 
-    yield_bin = double(yieldmap(ii));
     potentialyield_bin = double(potentialyield(ii));
-    irr_bin = double(avgpercirr(ii));
-    nfert_bin = double(nfert(ii));
-    pfert_bin = double(pfert(ii));
-    kfert_bin = double(kfert(ii));
-    desiredyield_bin = double(desiredyield(ii));
-    error_bin = double(errormap(ii));
-    
+    irr_bin = avgpercirr(ii);
+    nfert_bin = nfert(ii);
+    pfert_bin = pfert(ii);
+    kfert_bin = kfert(ii);
+    yield_bin = yieldmap(ii);
+    desiredyield_bin = desiredyield(ii);
+    moddesiredyield_bin = moddesiredyield(ii);
+        
     % identify potential and min yields in the bin
     potyieldbin = MS.potential_yield(bin);
     minyieldbin = MS.minimum_yield(bin);
     
     % initialize the lim_bin codes and nutrient plus vectors
-    lim_bin = zeros(length(yield_bin),1);
-    dN_bin = zeros(length(yield_bin),1);
-    dNQ_bin = zeros(length(yield_bin),1);
-    dP_bin = zeros(length(yield_bin),1);
-    dPQ_bin = zeros(length(yield_bin),1);
-    dK_bin = zeros(length(yield_bin),1);
-    dKQ_bin = zeros(length(yield_bin),1);
-    dI_bin = zeros(length(yield_bin),1);
-    dIQ_bin = zeros(length(yield_bin),1);
-    
-    %     nfertplus_bin = [];
-    %     kfertplus_bin = [];
-    %     pfertplus_bin = [];
-    %     irrplus_bin = [];
+    lim_bin = zeros(length(potentialyield_bin),1);
+    dN_bin = zeros(length(potentialyield_bin),1);
+    dNQ_bin = zeros(length(potentialyield_bin),1);
+    dP_bin = zeros(length(potentialyield_bin),1);
+    dPQ_bin = zeros(length(potentialyield_bin),1);
+    dK_bin = zeros(length(potentialyield_bin),1);
+    dKQ_bin = zeros(length(potentialyield_bin),1);
+    dI_bin = zeros(length(potentialyield_bin),1);
+    dIQ_bin = zeros(length(potentialyield_bin),1);
     
     % get b_nut (aka alpha for MB model)
     bnut = MS.b_nut(bin);
@@ -109,7 +155,8 @@ for bin = 1:100
         alpha = bnut;
     end
     
-    % grab the explanatory variable list
+    % grab the explanatory variable list (named "cb" although I can't
+    % remember why it has that name)
     cb = num2str(MS.explanatory_variable_list{bin});
     % add missing zeros if necessary
     if length(cb) < 4
@@ -121,7 +168,8 @@ for bin = 1:100
         end
     end
     
-    %%% cycle through cb, look at each individual functional form
+    % cycle through explanatory variables and look at each individual
+    % functional form
     
     % examine for nitrogen limitation
     if str2num(cb(1)) == 1
@@ -134,11 +182,11 @@ for bin = 1:100
         dNQ_bin = ones(length(dNQ_bin),1)+1;
     end
     switch modelnumber
-        case 1 % VL ELM
+        case 1 % VL LM
             nfertplus_bin = (log((potyieldbin ./ ...
-                desiredyield_bin) - 1) - bnut) ./ - bFitw;
-        case 2 % VL MB
-            nfertplus_bin = log((1 - (desiredyield_bin ./ ...
+                moddesiredyield_bin) - 1) - bnut) ./ - bFitw;
+        case 2 % VL MBM
+            nfertplus_bin = log((1 - (moddesiredyield_bin ./ ...
                 potyieldbin)) ./ bnut) ./ -bFitw;
     end
     dN_bin = nfertplus_bin - nfert_bin;
@@ -146,8 +194,7 @@ for bin = 1:100
     dN_bin(jj) = NaN;
     dNQ_bin(jj) = 4;
     if bininfo == 1
-        jj = find(nfertplus_bin > nfert_bin);
-        lim_bin(jj) = 1;
+        lim_bin(nfertplus_bin > nfert_bin) = 1;
     end
     
     % examine for phosphorus limitation
@@ -161,11 +208,11 @@ for bin = 1:100
         dPQ_bin = ones(length(dPQ_bin),1)+1;
     end
     switch modelnumber
-        case 1 % VL ELM
+        case 1 % VL LM
             pfertplus_bin = (log((potyieldbin ./ ...
-                desiredyield_bin) - 1) - bnut) ./ - bFitw;
-        case 2 % VL MB
-            pfertplus_bin = log((1 - (desiredyield_bin ./ ...
+                moddesiredyield_bin) - 1) - bnut) ./ - bFitw;
+        case 2 % VL MBM
+            pfertplus_bin = log((1 - (moddesiredyield_bin ./ ...
                 potyieldbin)) ./ bnut) ./ -bFitw;
     end
     dP_bin = pfertplus_bin - pfert_bin;
@@ -173,8 +220,7 @@ for bin = 1:100
     dP_bin(jj) = NaN;
     dPQ_bin(jj) = 4;
     if bininfo == 1
-        jj = find(pfertplus_bin > pfert_bin);
-        lim_bin(jj) = 1;
+        lim_bin(pfertplus_bin > pfert_bin) = 1;
     end
     
     % examine for potash limitation
@@ -188,11 +234,11 @@ for bin = 1:100
         dKQ_bin = ones(length(dKQ_bin),1)+1;
     end
     switch modelnumber
-        case 1 % VL ELM
+        case 1 % VL LM
             kfertplus_bin = (log((potyieldbin ./ ...
-                desiredyield_bin) - 1) - bnut) ./ - bFitw;
-        case 2 % VL MB
-            kfertplus_bin = log((1 - (desiredyield_bin ./ ...
+                moddesiredyield_bin) - 1) - bnut) ./ - bFitw;
+        case 2 % VL MBM
+            kfertplus_bin = log((1 - (moddesiredyield_bin ./ ...
                 potyieldbin)) ./ bnut) ./ -bFitw;
     end
     dK_bin = kfertplus_bin - kfert_bin;
@@ -200,8 +246,7 @@ for bin = 1:100
     dK_bin(jj) = NaN;
     dKQ_bin(jj) = 4;
     if bininfo == 1
-        jj = find(kfertplus_bin > kfert_bin);
-        lim_bin(jj) = 1;
+        lim_bin(kfertplus_bin > kfert_bin) = 1;
     end
     
     % examine for water limitation
@@ -209,11 +254,11 @@ for bin = 1:100
         bFitw(1) = str2num(MS.b_irr{bin});
         bFitw(2) = str2num(MS.c_irr{bin});
         switch modelnumber
-            case 1 % VL ELM
+            case 1 % VL LM
                 irrplus_bin = (log((potyieldbin ./ ...
-                    desiredyield_bin) - 1) - bFitw(1)) ./ - bFitw(2);
-            case 2 % VL MB
-                irrplus_bin = log((1 - (desiredyield_bin ./ ...
+                    moddesiredyield_bin) - 1) - bFitw(1)) ./ - bFitw(2);
+            case 2 % VL MBM
+                irrplus_bin = log((1 - (moddesiredyield_bin ./ ...
                     potyieldbin)) ./ bFitw(1)) ./ -bFitw(2);
         end
         % record water and nutrient limited areas
@@ -234,14 +279,8 @@ for bin = 1:100
     end
     
     % place lim_bin into yieldlim map
-    if errorflag == 1
-        jj = find((desiredyield_bin - error_bin) > potentialyield_bin);
-        lim_bin(jj) = 4;
-    elseif errorflag == 0
-        jj = find(desiredyield_bin > potentialyield_bin | yield_bin ...
-            > desiredyield_bin);
-        lim_bin(jj) = 4;
-    end
+    lim_bin(desiredyield_bin < yield_bin) = 4;
+    lim_bin(potentialyield_bin < yield_bin) = 5;
     yieldlim(ii) = lim_bin;
     dN(ii) = dN_bin;
     dNquality(ii) = dNQ_bin;
@@ -252,3 +291,5 @@ for bin = 1:100
     dI(ii) = dI_bin;
     dIquality(ii) = dIQ_bin;
 end
+
+%yieldlim(yieldlim = 0) = NaN;

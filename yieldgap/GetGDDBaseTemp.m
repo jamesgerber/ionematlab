f46ef85c80092f9d1df0d9bc17d968ac1354247c
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-05-02 16:24:09 +0000

    yieldgapchanges
    git-svn-id: file:///Users/svnuser/matlab@373 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldgap/GetGDDBaseTemp.m
+++ yieldgap/GetGDDBaseTemp.m
@@ -1,3 +1,19 @@
 function T=GetGDDBaseTemp(crop)
 % return base GDD temperature
-T='0';
+
+persistent C
+
+if isempty(C)
+    C=readgenericcsv('croptype_NPK.csv',2);
+end
+
+iirow=strmatch(crop,C.CROPNAME,'exact');
+
+if numel(iirow)~=1
+   T='0';
+   warning(['found multiple (or zero) matches for cropname ' crop ' in ' mfilename]);
+   return
+end
+
+
+T=num2str(C.GDD_Base_Temp(iirow));

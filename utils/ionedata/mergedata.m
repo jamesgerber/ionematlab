dc050e2cba098f7e897cb587a71350ac43909581
andrew <andrew@1e732962-677d-4dd9-a278-707af5f4a22e>
2011-07-08 20:21:11 +0000

    Useful data merge function added
    git-svn-id: file:///Users/svnuser/matlab@421 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/ionedata/mergedata.m
+++ utils/ionedata/mergedata.m
@@ -1,4 +1,16 @@
 function A=mergedata(background,blon,blat,data,dlon,dlat,method)
+%  mergedata - paste data onto background
+%
+%  AddStates(background,blon,blat,data,dlon,dlat), where background is some
+%  background data like a global land mask, with longitude and latitude
+%  defined by blon and blat, and data is an array to be pasted on top of it
+%  with latitude and longitude defined by dlon and dlat, will put data over
+%  background such that the resulting array will have the same extent and
+%  same lat-long values as background. The extent of data must be within
+%  the extent of background. NaNs within data will be treated as
+%  transparent and will not be pasted.
+%
+
 if (nargin<7)
     method='nearest';
 end
@@ -9,11 +21,6 @@ numlon=abs(ilon1-ilon2)+1;
 [~,ilat2]=nearestelement(blat,max(dlat));
 numlat=abs(ilat1-ilat2)+1;
 data=easyinterp2(data,numlon,numlat,method);
-size(data)
-ilon1
-ilon2
-ilat1
-ilat2
 if (ilon1>ilon2)
     tmp=ilon1;
     ilon1=ilon2;
@@ -24,5 +31,7 @@ if (ilat1>ilat2)
     ilat1=ilat2;
     ilat2=tmp;
 end
+tmp=background;
 background(ilon1:ilon2,ilat1:ilat2)=data;
+background(isnan(background))=tmp(isnan(background));
 A=background;
--- utils/ionedata/nearestelement.m
+++ utils/ionedata/nearestelement.m
@@ -1,3 +1,4 @@
 function [a,i]=nearestelement(A,x)
+% NEARESTELEMENT - return the nearest element to x of array A
 [~,i]=min(abs(A-x));
 a=A(i);

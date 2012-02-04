cd4a8d23e025e03727bf650ff67a5383da6273f1
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2012-02-04 16:50:41 +0000

    working with hotspot
    git-svn-id: file:///Users/svnuser/matlab@515 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionedata/tradeoffplot.m
@@ -0,0 +1,5 @@
+plot(TO.badquantitysorted/max(TO.badquantitysorted),TO.goodquantitysorted/max(TO.goodquantitysorted))
+xlabel(' bad thing ')
+ylabel(' good thing ')
+grid on
+title([' Bad thing used to produce a good thing '])

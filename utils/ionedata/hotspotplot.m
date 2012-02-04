a4554af3791ab1a636ec7bbd92fb3ca20cab6717
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2012-02-04 16:50:17 +0000

    working with hotspot
    git-svn-id: file:///Users/svnuser/matlab@514 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/ionedata/hotspotplot.m
@@ -0,0 +1,5 @@
+plot(HS.badquantitysorted/max(HS.badquantitysorted),HS.goodquantitysorted/max(HS.goodquantitysorted))
+xlabel(' bad thing ')
+ylabel(' good thing ')
+grid on
+title([' Bad thing used to produce a good thing '])

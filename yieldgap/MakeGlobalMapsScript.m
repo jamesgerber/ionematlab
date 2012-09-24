1bc95bfdf9d9c7d0ba7bdad14489f2639e69cfb2
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2012-09-24 19:44:05 +0000

    slowly updating yieldgaps while i test svnbackups
    git-svn-id: file:///Users/svnuser/matlab@621 1e732962-677d-4dd9-a278-707af5f4a22e

--- yieldgap/MakeGlobalMapsScript.m
+++
@@ -1,6 +0,0 @@
-
-%TitleStr=
-titlestr=([cropname 'Yield Gap. ' Nstr 'x' Nstr '. Moisture index=' ...
-    WetFlag '. GDD Base Temp=' GDDTempstr '.  Rev' Rev]);
-
-FastSurf(Long,Lat,YieldGapArray,'Yield Gap',titlestr);

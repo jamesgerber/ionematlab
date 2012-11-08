fe732dcb752d2378210d4a667800c88f06a18d86
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2012-11-08 17:08:07 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@639 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/WATCH/pullwatchtimeseries.m
+++ utils/WATCH/pullwatchtimeseries.m
@@ -1,18 +1,39 @@
-function [mdn,ts]=pullwatchtimeseries(idx,basedir);
+function [mdnvect,ts]=pullwatchtimeseries(idx,basedir);
 % pullwatchtimeseries
 
 
-idx=1:1000;
-basedir=[iddstring '/Climate/reanalysis/WATCH/Tair_WFD/'];
+Ntimeseries=1:(365.25*100*8);
 
+Nindices=length(find(idx));
+ts=NaN(1:Nindices,1:Ntimeseries);
 
-for mm=1:12
-    for yy=1902:2000;
-        FileName=[basedir 'Tair_WFD_' int2str(yy) int2str(mm) '.nc']
+mdnvect=[];
+
+%basedir=[iddstring '/Climate/reanalysis/WATCH/Tair_WFD/'];
+basedir='./'
+
+for yy=1902:1902;
+            mdn0=datenum(yy,01,01,00,00,00);  % note this should be hardwired to Jan
+
+    for mm=1:12
+        FileName=[basedir 'Tair_WFD_' int2str(yy) sprintf('%02d',mm) '.nc']
         S=OpenGeneralNetCDF(FileName);
         
-        keyboard
+        % initial date string
+          x=S(4).Attributes(4).attrvalue;
+        
+        
+        mdn0=datenum(yy,01,01,00,00,00);  % note this should be hardwired to Jan
+        mdn=mdn0+S(4).Data/(24*3600);  %mdn in units of days.  so go from seconds to days.
+       % datestr(double(mdn(1)))
+        tstmp=S(6).Data(idx,:);
+        
+        kk=(length(mdnvect)+1 ):length(mdnvect)+length(mdn);
+        
+        ts(1:Nindices,kk)=tstmp;
+        
+        mdnvect=[ mdnvect ; mdn];
+        
     end
 end
 
-        

9ac8e02921a6fa4a721e4e4dbab731dcd5306fe2
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2013-02-11 23:01:31 +0000

    capitalization
    git-svn-id: file:///Users/svnuser/matlab@694 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/zcapnames/SingleAllVars.m
+++ utils/zcapnames/SingleAllVars.m
@@ -1,25 +1,11 @@
-function SingleAllVars
-% SingleAllVars - Turn all single valued variables to double
-%
-%
-%   Syntax
-%       SingleAllVars  with no arguments will examine all variables in the
-%       base workspace, and take those of class 'single' and make them
-%       'double.'   It's useful when trying to use tools (e.g. graphics
-%       tools) which require double precision inputs on a set of data that
-%       is all single.  
-%
-%       This is a development tool: it is a bad idea to use this within any
-%       function or script.
-
-
-% JSG
-% Institute on the Environment
-% October, 2009
-
-a=evalin('caller','whos');
-for j=1:length(a);
-    if isequal(a(j).class,'double')
-        evalin('caller',[a(j).name '=single(' a(j).name ');']);
-    end
+function varargout=helperfunction(varargin)
+% helper function to deal with capitalization issue
+persistent madewarning
+if isempty(madewarning)
+    madewarning=1;
+    disp(['calling ' lower(mfilename) ' (no caps)']);
 end
+[varargout{1:nargout}]=feval(lower(mfilename),varargin{:});
+
+
+

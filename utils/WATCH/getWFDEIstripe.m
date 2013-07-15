f41ef6137bffba5ea2fc1d57672bc4445b3f9553
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2013-07-15 03:55:56 +0000

    getWFDEIstripe '
    git-svn-id: file:///Users/svnuser/matlab@734 1e732962-677d-4dd9-a278-707af5f4a22e

---
+++ utils/WATCH/getWFDEIstripe.m
@@ -0,0 +1,25 @@
+function [mdnvect,ts]=getWFDEIstripe(idx,type);
+% getWFDEIstripe
+%
+%   getWFDEItimeseries(getWFDEIstripe,type)  where type can be
+%   'Rainf_WFDEI_CRU		Tair_WFDEI
+%   Snowf_WFDEI_CRU		Tair_daily_WFDEI
+%
+
+%% code to make stripes
+
+%WFDEIVar='Tair_daily_WFDEI';
+WFDEIVar=type;
+
+basedir = [iddstring 'Climate/reanalysis/WFDEI/stripes'];
+
+for j=1:length(idx);
+        FileName=[basedir '/' WFDEIVar '/' WFDEIVar int2str(idx(j))];
+        x=load(FileName)
+     %   mdnvect=x;
+end
+
+mdnvect=x.mdnvect;
+ts=x.tsvect;
+
+

52ead2b28503a4eddbf6ce0b5d7c6c655637e263
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2013-09-06 16:49:49 +0000

    message here
    git-svn-id: file:///Users/svnuser/matlab@745 1e732962-677d-4dd9-a278-707af5f4a22e

--- irrigation/ReadSeibertIrrigationFiles.m
+++ irrigation/ReadSeibertIrrigationFiles.m
@@ -1,3 +1,80 @@
+function ReadSeibertIrrigationFiles(filename)
+% ReadSeibertIrrigationFiles
+%
+%  reads .asc files with this header
+%ncols         4320
+%nrows         2160
+%xllcorner     -180
+%yllcorner     -90
+%cellsize      0.083333
+%NODATA_value  -9
+%
+%  syntax
+%
+%  ReadSeibertIrrigationFiles will open all .asc files in directory, save
+%  as a .mat
+
+if nargin==0
+    a=dir('*.ASC');
+    for j=1:length(a)
+        a(j).name
+        ReadSeibertIrrigationFiles(a(j).name);
+    end
+    a=dir('*.asc');
+    for j=1:length(a)
+        ReadSeibertIrrigationFiles(a(j).name);
+    end
+    return
+end
+
+
+b=dlmread([filename],' ',6,0);
+%end
+
+NewData=datablank;;
+
+% b now has an extra line. (4321 instead of 4320)
+
+% kate b and jamie g think that this is artefact of reading/writing
+% different formats and it is the last line that doesn't correspond to
+% anything physical. also, noted that for sugarcane there is consistent
+% location along india border if we remove that final line.
+% align
+
+% so, just take the first 4320 of b.  also taking transpose
+NewData(1:4320,:)=b(:,1:4320)';
+
+% ii=findstr(ThisName,'MM');
+% MIRCACROPNo=str2num(ThisName(ii+ (4:5)));
+
+ii=find(filename=='.');
+
+NewName=filename(1:ii-1);
+
+
+
+
+DAS.MissingDataValue=-9;
+DAS.DateProcessed=datestr(now)
+DAS.Notes='Do not distribute.';
+
+[Long,Lat]=InferLongLat(NewData);
+
+WriteNetCDF(Long,Lat,single(NewData),'Data',NewName,DAS);
+!gzip *.nc
+
+Data=single(NewData);
+
+save(NewName,'Data','filename','DAS')
+
+
+return
+
+%%%
+
+%%%% script that was an earlier version of this
+
+
 FileDir=[iddstring 'Irrigation/Seibert/Seibert_Irrigation_Jan2011_Raw/'];
 wd=pwd;
 
@@ -24,18 +101,18 @@ for j=55:length(a);
     
     
     % need to choose one of these lines
-    NewData(1:4320,:)=b(:,1:4320)'; 
+    NewData(1:4320,:)=b(:,1:4320)';
     
-   % ii=findstr(ThisName,'MM');
-   % MIRCACROPNo=str2num(ThisName(ii+ (4:5)));
+    % ii=findstr(ThisName,'MM');
+    % MIRCACROPNo=str2num(ThisName(ii+ (4:5)));
     
     ii=find(ThisName=='.');
     
     NewName=ThisName(1:ii-1);
     
     
-  %  NewFileName=['blahblahblah_MIRCACropCode' num2str(MIRCACROPNo) ...
-  %      '.nc'];
+    %  NewFileName=['blahblahblah_MIRCACropCode' num2str(MIRCACROPNo) ...
+    %      '.nc'];
     
     DAS.MissingDataValue=-9;
     DAS.DateProcessed=datestr(now)

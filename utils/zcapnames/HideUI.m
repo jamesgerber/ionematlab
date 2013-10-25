d0691942bc31cbbfee729e705e19108509fc19e7
jsgerber <jsgerber@1e732962-677d-4dd9-a278-707af5f4a22e>
2013-10-25 16:28:58 +0000

    capnamesthing
    git-svn-id: file:///Users/svnuser/matlab@763 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/zcapnames/HideUI.m
+++ utils/zcapnames/HideUI.m
@@ -1,13 +1,11 @@
-function HideUI;
-% HIDEUI - hide uicontrols in the current figure
-%
-%  See Also ShowUI
-Hfig=gcf;
-
-h=get(Hfig,'children');
-for j=1:length(h)
-  if isequal( get(h(j),'type'),'uicontrol')
-    set(h(j),'Visib','off');
-  end
+function varargout=helperfunction(varargin)
+% helper function to deal with capitalization issue
+persistent madewarning
+if isempty(madewarning)
+    madewarning=1;
+    disp(['calling ' lower(mfilename) ' (no caps)']);
 end
+[varargout{1:nargout}]=feval(lower(mfilename),varargin{:});
+
+
 

4508b39686696eea996e88bf42c56898753d21ac
svnuser <svnuser@1e732962-677d-4dd9-a278-707af5f4a22e>
2015-09-09 19:02:24 +0000

    git-svn-id: file:///Users/svnuser/matlab@1243 1e732962-677d-4dd9-a278-707af5f4a22e

--- utils/ionedata/opengeneralnetcdf.m
+++
@@ -1,9 +0,0 @@
-function Svector=OpenGeneralNetCDF(varargin)
-persistent madewarning
-
-if isempty(madewarning)
-    disp(['calling version with (no caps)']);
-    madewarning=1;
-end
-
-Svector=opengeneralnetcdf(varargin{:});

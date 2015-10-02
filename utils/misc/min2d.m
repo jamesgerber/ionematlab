075d8f930e761a280d60766c4324e4f67a69ad5d
Mariana Ogg <oggxx008@x-10-104-161-244.uofm-secure.wireless.umn.edu>
2015-10-02 11:07:37 -0500

    changes made to min2d

--- utils/misc/min2d.m
+++ utils/misc/min2d.m
@@ -1,21 +1,43 @@
-function [minval,RowIndex,ColumnIndex]=min2d(z)
-% Min2d - returns min value, row index, and column index
+function [MinVal,RowIndex,ColumnIndex]=min2d(z)
+% MIN2D - returns min value of 2d matrix, and index
+%
+%  Can return either row/column indices or a single matrix index depending
+%  on the output syntax
 %
 %  SYNTAX
 %         [MinVal,RowIndex,ColumnIndex]=min2d(z);
-%
+%         [MinVal,MatrixIndex]=max2d(z);
 %
 %  example
 %
 %          z=rand(5,12);
-%          z(3,1)=2.7;
+%          z(3,4)=2.7;
 %          [MinVal,RowIndex,ColumnIndex]=min2d(z)
 %          z(RowIndex,ColumnIndex)
-%           
-% location of minimum value:
+%
+%          z(3,4)=2.7;
+%          [MinVal,MatrixIndex]=min2d(z)
+%          z(MatrixIndex)
+%
+
+%  J. Gerber
+%  Global Landscapes Initiative
+%  University of Minnesota
+
+
+switch nargout
+    case {0,1}
+        
+        minval=min(z(:));
+    case 2
+        [minval,RowIndex]=min(z(:));
+        % RowIndex doesn't actually equal row index here - it's a matrix
+        % index
+    case 3
+    
     [mincolval,colindex]=min(z);
     [minval,rowindex]=min(mincolval);
   
     RowIndex=colindex(rowindex);
     ColumnIndex=rowindex;
-    
+end

d2ff999f5be122b2659496b16fb34eb28028db26
Mariana Ogg <oggxx008@x-128-101-92-173.cfans.umn.edu>
2015-10-08 10:28:01 -0500

    switched to lower case

--- utils/ionegraphics/nsg.m
+++ utils/ionegraphics/nsg.m
@@ -6,16 +6,16 @@ function OS=NSG(varargin);
 %  See also NiceSurfGeneral
 
 if nargin==0
-    help('NiceSurfGeneral')
+    help('nicesurfgeneral')
     return
 end
 
 if nargin==1
     % only syntax that can lead to this is calling with a single variable
     NSS.titlestring=inputname(1)
-    OS=NiceSurfGeneral(varargin{1:end},NSS);
+    OS=nicesurfgeneral(varargin{1:end},NSS);
     showui
 else
-    OS=NiceSurfGeneral(varargin{1:end});
+    OS=nicesurfgeneral(varargin{1:end});
 end
 

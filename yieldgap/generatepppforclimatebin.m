4bd8252f264a11518b6ca72839d34628f2a7f244
james gerber <jgerber68@gmail.com>
2015-12-07 09:18:26 -0600

    new code

--- yieldgap/generatepppforclimatebin.m
+++ yieldgap/generatepppforclimatebin.m
@@ -1,5 +1,8 @@
-%function data=generatepppforclimatebin(binnumber)
-binnumber=1
+function data=generatepppforclimatebin(binnumber)
+%binnumber=1
+
+% some settings
+
 
 %%%%
 % This function uses the 5x5 climate bins and the 5x5 maize yield gaps to
@@ -11,6 +14,10 @@ binnumber=1
 
 % 12/04/2015, MO, Global Landscapes Initiative, Institute on the
 % Environment, University of Minnesota
+
+
+
+PPPMax=60000;
 
 %%%%LOAD DATA HERE%%%%%
 %load global gdp purchasing power parity
@@ -27,25 +34,70 @@ yg=OS.YieldGapFraction;
 % a meaning area
 a=OS.Area;
 
+%% first make map - then we can pull out colormap
+
 
 select_bin=(bin==binnumber);
-
-%generate histogram
-figure
-hist(gdp(select_bin))
 titlestr = sprintf('Climate bin %d',binnumber);
-title(titlestr);
+%title(titlestr);
 
 %generate map
 bin_ppp = (gdp.*select_bin);
 bin_ppp = +bin_ppp;
 bin_ppp(bin_ppp==0)=NaN;
 NSS.Title = titlestr;
-nsg(bin_ppp,NSS)
+NSGS=nsg(bin_ppp,NSS,'caxis',[0 PPPMax]);
 
-%generate area weighted histogram
-aw=awhist(gdp,a,1:5000:60000);
-figure
-bar(aw.bincenters,aw.distbyweight);
+
+
+
+%% area-weighted histogram
+
+% %generate histogram
+% figure
+% hist(gdp(select_bin))
+% titlestr = sprintf('Climate bin %d',binnumber);
+% title(titlestr);
+
+
+
+%%generate area weighted histogram
+%aw=awhist(gdp,a,1:5000:PPPMax); % Mariana - this was wrong.  next line limited to bin.
+aw=awhist(gdp(select_bin),a(select_bin),1:5000:PPPMax);
+figure(99)
+
+%axes(hax);
+    [jp,EES]=subplot_mp(5,5,binnumber,1);
+for j=1:length(aw.bincenters);
+
+    x=aw.bincenters(j);
+    y=aw.distbyweight(j);
+    
+    hl=line([x x],[0 y]);
+   % hl=bar([x ],[ y],4);
+    hold on
+    N=size(NSGS.cmap_final,1);
+    ThisColorIndex= floor(N* (x-NSGS.coloraxis(1))/(NSGS.coloraxis(2)-NSGS.coloraxis(1)))+1;
+    ThisColorIndex=min(ThisColorIndex,N-1);  % can't over run, and last value is no data
+    ThisColorIndex=max(ThisColorIndex,2);  % can't underrun, and first value is ocean
+    
+    newcolor=NSGS.cmap_final(ThisColorIndex,:);
+    
+    set(hl,'Color',newcolor,'linewidth',2);
+end
+hold off
+    
+%bar(aw.bincenters,aw.distbyweight);
+
+
+
+
 titlestr = sprintf('Area weighted, Climate bin %d',binnumber);
-title(titlestr);
+
+
+return
+%%
+for j=1:25
+    generatepppforclimatebin(j)
+    pause   
+end

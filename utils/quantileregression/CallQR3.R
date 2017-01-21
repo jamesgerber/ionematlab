a95d4d729bfcde0dd8cde196fcabf760a4ffd55c
jsgerber <jsgerber@umn.edu>
2017-01-20 23:43:30 -0600

    subversion sucks

---
+++ utils/quantileregression/CallQR3.R
@@ -0,0 +1,75 @@
+
+# Jamie code to carry out quantile regression using quantreg package in R. 
+# this reads in datafile.csv, which must evaulate to an array whose first column is Yield and whose second column is weights.  The next N columns are sent in as variables to the QR analysis.
+
+# first read in datafile.csv file with y,x1,x2,x3 ... etc.
+
+#setwd("/Users/jsgerber/sandbox/jsg131_yieldgapsovertime_withQR_and_bins")
+
+#############
+####Data#####
+#############
+library(R.matlab)
+newdata<-readMat("transferdatatoR.mat")
+data2<-newdata$BigArray
+
+tauvals<-newdata$tauvalues
+tt=tauvals[1,1]
+##data<-read.csv("datafile.csv")
+
+Ncols<-dim(data2)[2]
+#Yield<-data[,1]
+Yield<-data2[,1]
+W<-data2[,2]
+
+# definining named variables
+for (j in 3:Ncols)
+{
+	#print(j)	
+eval(parse(text=paste("var",as.character(j-2),"<-data2[,j]",sep="")))
+}
+                   
+
+####Tested quantiles####
+library("quantreg")
+
+if (Ncols==4)
+Fit.i<-rq(Yield~var1+var2,tau=tt,weight=W,ci=TRUE)
+if (Ncols==5)
+Fit.i<-rq(Yield~var1+var2+var3,tau=tt,weight=W,ci=TRUE)
+if (Ncols==6)
+Fit.i<-rq(Yield~var1+var2+var3+var4,tau=tt,weight=W,ci=TRUE)
+if (Ncols==7)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5,tau=tt,weight=W,ci=TRUE)
+if (Ncols==8)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6,tau=tt,weight=W,ci=TRUE)
+if (Ncols==9)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7,tau=tt,weight=W,ci=TRUE)
+if (Ncols==10)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8,tau=tt,weight=W,ci=TRUE)
+if (Ncols==11)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9,tau=tt,weight=W,ci=TRUE)
+if (Ncols==12)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10,tau=tt,weight=W,ci=TRUE)
+if (Ncols==13)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11,tau=tt,weight=W,ci=TRUE)
+if (Ncols==14)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12,tau=tt,weight=W,ci=TRUE)
+if (Ncols==15)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13,tau=tt,weight=W,ci=TRUE)
+if (Ncols==16)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14,tau=tt,weight=W,ci=TRUE)
+if (Ncols==17)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15,tau=tt,weight=W,ci=TRUE)
+if (Ncols==18)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16,tau=tt,weight=W,ci=TRUE)
+if (Ncols==19)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17,tau=tt,weight=W,ci=TRUE)
+if (Ncols==20)
+Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18,tau=tt,weight=W,ci=TRUE)
+
+	Theta<-coef(Fit.i)
+	print(Theta)
+	write(Theta,file='output.txt',ncolumns=1)
+AICValue<-AIC(Fit.i)
+write(AICValue,file='AICValue.txt',ncolumns=1);
---
+++ utils/quantileregression/CallR_frommatlab_function.m
@@ -0,0 +1,78 @@
+function [theta,theta_lowerbd,theta_upperbd,AIC] =...
+    CallR_frommatlab_function(Y,W,VarStruct,modelterms,tauvalues,iikeep);
+% Y - Nx1 column of yields
+% W - column of weights
+% VarStruct - structure of variables which will be put into the workspace
+% here in the function
+% modelterms - these will 'eval' to the variable names that R will look for
+% tauvalues - values for QR
+% iikeep = indices to keep.  Should have same length as Y
+
+if nargin <5
+    tauvalues=0.95
+end
+
+if nargin < 6
+    iikeep=1:length(Y);
+end
+% this function is an embarassing disaster - step through in debugger to
+% see what it does.
+
+fileheaderline=['Y,W'];
+M=[W];
+
+expandstructure(VarStruct);
+
+% constructing this for call to R.  but R call doesn't want the column of
+% ones.
+for j=1:length(modelterms)-1
+    newvar=eval(modelterms{j+1});
+    newvarname=['var' int2str(j)];
+    fileheaderline=[fileheaderline ',' newvarname];
+    M=[M newvar];
+end
+
+
+unix('rm output.txt')
+unix('rm AICValue.txt');
+%M=[ W X1(:) X2(:)];
+
+%tic
+%writefile('datafile.csv',Y,M,fileheaderline,',');
+%toc
+
+tic
+BigArray=[Y M];
+BigArray=BigArray(iikeep,:);
+
+
+
+
+
+save transferdatatoR.mat  -v6 BigArray tauvalues
+toc;
+
+disp(['calling R program']);
+% tic
+% [s,w]=unix('R CMD BATCH /Users/jsgerber/source/matlabgit/matlab/utils/quantileregression/CallQR2.R Routput.txt')
+% unix('cat Routput.txt')
+% theta=load('output.txt')
+% toc
+
+tic
+[s,w]=unix('R CMD BATCH /Users/jsgerber/source/matlab/trunk/utils/quantileregression/CallQR3.R Routput.txt');
+%unix('cat Routput.txt')
+N=length(modelterms);
+thetatemp=load('output.txt');
+theta=thetatemp(1:N);
+
+AIC=load('AICValue.txt');
+if length(thetatemp)==3*N
+    theta_lowerbd=thetatemp(N+1:2*N);
+    theta_upperbd=thetatemp(2*N+1:3*N);
+else
+    theta_lowerbd=NaN;
+    theta_upperbd=NaN;
+end
+ 
+toc

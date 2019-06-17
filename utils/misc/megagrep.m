dc33f0a1320fae2db00f4c74f53efbe520cbe15d
stiff030 <stiff030@x-128-101-92-167.cfans.umn.edu>
2019-06-17 15:01:29 -0500

    Uploaded memoryLeft, fixed megagrep

--- utils/misc/megagrep.m
+++ utils/misc/megagrep.m
@@ -1,4 +1,4 @@
-function MegaGrep(varargin) 
+function megagrep(varargin) 
 %MEGAGREP - execute a shell command in every directory on path
 %
 % Execute a dos command in every directory on the path which
@@ -8,13 +8,13 @@ function MegaGrep(varargin)
 %
 %  SYNTAX
 %          (Single Argument)
-%          MegaGrep('ls | grep -i certainfile ')  Goes to every directory 
+%          megaGrep('ls | grep -i certainfile ')  Goes to every directory 
 %          on the path (whose name doesn't contain "toolbox") and executes
 %          'ls | grep -i certainfile' in a dos shell.  results will 
 %          be printed to the screen.
 %
 %          (multiple Arguments)
-%          Megagrep -i CertainVariable *.m    executes "grep -i CertainVariable *.m" 
+%          megagrep -i CertainVariable *.m    executes "grep -i CertainVariable *.m" 
 % 
 %  EXAMPLE        
 %          megagrep(' ''Maximum Generator Force'' ','*.m')
@@ -26,19 +26,22 @@ function MegaGrep(varargin)
 % See Also: MegaEdit, ChebWin, UIPutFile
 %
 % AUTHOR:  James Gerber, with the spiritual guidance of Chris Griffin.
+% Edited by: Sam Stiffman, 6/17/2019
 
 if nargin==0
    help megagrep
    return
-end
-if nargin==1
+elseif nargin==1
    dosargs=varargin{1};
 else
-   str=[];for j=1:length(varargin);str=[str varargin{j} ' '];end
+   str=[];
+   for j=1:length(varargin)
+       str=[str varargin{j} ' '];
+   end
    dosargs=['grep ' str];
 end
 
-wd=pwd;
+workingDir=pwd;
 ListOfDirsToSearch={};   
 
 PathList=path;
@@ -48,9 +51,9 @@ PathList=path;
 PathList=[pathsep PathList pathsep pwd pathsep];
 ii=find(PathList==pathsep);
 
-for j=1:length(ii)-1;
+for j=1:length(ii)-1
    ThisPath=PathList(  (ii(j)+1) : (ii(j+1))-1 );
-   if isempty(findstr('toolbox',ThisPath))
+   if ~contains(ThisPath,'toolbox')
       ListOfDirsToSearch{end+1}=ThisPath;   
    %look to see if there is a private directory
       if exist(fullfile(ThisPath,'private',''),'dir')
@@ -59,33 +62,38 @@ for j=1:length(ii)-1;
    end
 end
 
-disp(['Searching in ...']);
+disp('Searching in ...');
 char(ListOfDirsToSearch)
 
 
-for j=1:length(ListOfDirsToSearch);
+for j=1:length(ListOfDirsToSearch)
     
     nextdir=ListOfDirsToSearch{j};
     
     if isequal(nextdir(1),'.')
-    cd(wd)
+    cd(workingDir)
     try
         cd(nextdir)
     catch
-        disp(['couldn''t find directory ' nextdir ' from ' wd ]);
+        disp(['couldn''t find directory ' nextdir ' from ' workingDir ]);
     end
     else
         cd(nextdir);
    
     end
-   disp('');
-   disp(pwd);
-   disp(['! ' dosargs  ]);
-   eval(['! ' dosargs  ]);
+    %POSIX commands have status 0 when successfull
+    [status, result] = system(dosargs);
+    if ~status
+        disp('')
+        disp(pwd)
+        disp(['! ' dosargs  ]);
+        disp(result)
+    end
+
    
 end
 
-cd(wd);
+cd(workingDir);
 
 
 

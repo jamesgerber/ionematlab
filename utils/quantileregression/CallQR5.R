d642c969550ebb18fc32ec4c08e27ec9cc653fbc
stiff030 <stiff030@umn.edu>
2020-03-31 14:24:16 -0500

    Fixed CallQR5.R
    renamed the old CallQR5.R as CallQR5_old.R

--- utils/quantileregression/CallQR5.R
+++ utils/quantileregression/CallQR5.R
@@ -8,10 +8,12 @@
 
 #setwd("/Users/jsgerber/sandbox/jsg131_yieldgapsovertime_withQR_and_bins")
 #  setwd("/Users/jsgerber/sandbox/jsg131_clean_ygot")
-
+# setwd("D:\\Workspaces\\Matlab Workspace") 
 #############
 ####Data#####
 #############
+
+
 library(R.matlab)
 newdata<-readMat("transferdatatoR.mat")
 data2<-newdata$BigArray
@@ -25,13 +27,18 @@ Ncols<-dim(data2)[2]
 Yield<-data2[,1]
 W<-data2[,2]
 
-# definining named variables
-for (j in 3:Ncols)
+# definining named variables and creating the formula
+
+compoundExpression <- "Yield~var1"
+eval(parse(text=paste("var",as.character(1),"<-data2[,3]",sep="")))
+for (j in 4:Ncols)
 {
-	#print(j)	
-eval(parse(text=paste("var",as.character(j-2),"<-data2[,j]",sep="")))
+  #print(j)	
+  eval(parse(text=paste("var",as.character(j-2),"<-data2[,j]",sep="")))
+  eval(parse(text=paste("compoundExpression<-paste(compoundExpression,\"+\",\"var",as.character(j-2),"\",collapse=\"\",sep=\"\")",collapse="", sep="")))
 }
-                   
+# print(compoundExpression)
+# print output: Yield~var1+var2+var3+...+varn
 alphaval<-newdata$alphavalue
 
 aa=alphaval[1,1]
@@ -50,119 +57,33 @@ library("quantreg")
 methodsflag<-newdata$algorithmflag
 
 
-if (methodsflag==0)
-methodstr<-"br"
 
-if (methodsflag==1)
-methodstr<-"fn"
 
-if (methodsflag==2)
-methodstr<-"pfn"
-
-if (methodsflag==3)
-methodstr<-"sfn"
+if (methodsflag==1){
+  methodstr<-"fn"
+} else if (methodsflag==2){
+  methodstr<-"pfn"
+} else if (methodsflag==3){
+  methodstr<-"sfn"
+}else{ 
+  #Defaults to "br"
+  methodstr<-"br"
+}
 
 
 if (methodstr=="br") {
-
-if (Ncols==3)
-Fit.i<-rq(Yield~var1,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==4)
-Fit.i<-rq(Yield~var1+var2,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==5)
-Fit.i<-rq(Yield~var1+var2+var3,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==6)
-Fit.i<-rq(Yield~var1+var2+var3+var4,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==7)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==8)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==9)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==10)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==11)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==12)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==13)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==14)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==15)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==16)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==17)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==18)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==19)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==20)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==21)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==22)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19+var20,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==23)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19+var20+var21,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-if (Ncols==24)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19+var20+var21+var22,tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
-
+  Fit.i<-rq(as.formula(compoundExpression),tau=tt,weight=W,ci=TRUE,alpha=aa,method=methodstr)
+  
 } else {
-if (Ncols==3)
-Fit.i<-rq(Yield~var1,tau=tt,weight=W,method=methodstr)
-if (Ncols==4)
-Fit.i<-rq(Yield~var1+var2,tau=tt,weight=W,method=methodstr)
-if (Ncols==5)
-Fit.i<-rq(Yield~var1+var2+var3,tau=tt,weight=W,method=methodstr)
-if (Ncols==6)
-Fit.i<-rq(Yield~var1+var2+var3+var4,tau=tt,weight=W,method=methodstr)
-if (Ncols==7)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5,tau=tt,weight=W,method=methodstr)
-if (Ncols==8)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6,tau=tt,weight=W,method=methodstr)
-if (Ncols==9)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7,tau=tt,weight=W,method=methodstr)
-if (Ncols==10)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8,tau=tt,weight=W,method=methodstr)
-if (Ncols==11)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9,tau=tt,weight=W,method=methodstr)
-if (Ncols==12)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10,tau=tt,weight=W,method=methodstr)
-if (Ncols==13)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11,tau=tt,weight=W,method=methodstr)
-if (Ncols==14)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12,tau=tt,weight=W,method=methodstr)
-if (Ncols==15)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13,tau=tt,weight=W,method=methodstr)
-if (Ncols==16)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14,tau=tt,weight=W,method=methodstr)
-if (Ncols==17)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15,tau=tt,weight=W,method=methodstr)
-if (Ncols==18)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16,tau=tt,weight=W,method=methodstr)
-if (Ncols==19)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17,tau=tt,weight=W,method=methodstr)
-if (Ncols==20)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18,tau=tt,weight=W,method=methodstr)
-if (Ncols==21)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19,tau=tt,weight=W,method=methodstr)
-if (Ncols==22)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19+var20,tau=tt,weight=W,method=methodstr)
-if (Ncols==23)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19+var20+var21,tau=tt,weight=W,method=methodstr)
-if (Ncols==24)
-Fit.i<-rq(Yield~var1+var2+var3+var4+var5+var6+var7+var8+var9+var10+var11+var12+var13+var14+var15+var16+var17+var18+var19+var20+var21+var22,tau=tt,weight=W,method=methodstr)
-
+  Fit.i<-rq(as.formula(compoundExpression),tau=tt,weight=W,method=methodstr)
+  
 }
 
 
 
-	Theta<-coef(Fit.i)
-	print(Theta)
-	write(Theta,file='output.txt',ncolumns=1)
+Theta<-coef(Fit.i)
+print(Theta)
+write(Theta,file='output.txt',ncolumns=1)
 AICValue<-AIC(Fit.i)
 BICValue<-BIC(Fit.i)
 print(BICValue)

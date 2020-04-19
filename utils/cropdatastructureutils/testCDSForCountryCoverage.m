0d89e3c65a79c7f8146226fc8ed33b5a80053f4a
james gerber <jsgerber@umn.edu>
2020-04-19 13:14:32 -0500

    minor CDS update

--- utils/cropdatastructureutils/testCDSForCountryCoverage.m
+++ utils/cropdatastructureutils/testCDSForCountryCoverage.m
@@ -2,24 +2,29 @@
 wd=pwd;
 
 cd /Users/jsgerber/sandbox/jsg162_worldbankCWON/CDSextrapolations
-
-a=dir('NewCDSwithAYRev4*');
+Revstr='Rev5'
+a=dir(['NewCDSwithAY' Revstr '*']);
 
 ymapcoverage=logical(datablank);
 amapcoverage=logical(datablank);
 
-
+for yr=[1990:5:2015];
+    
 for j=1:numel(a);
     load(a(j).name,'NewCDS');
     
-    [yield,area,deepakindexmap,CDSindexmap]=CDStoRasters_allocatetomap(NewCDS,2012);
+    [yield,area,deepakindexmap,CDSindexmap]=CDStoRasters_allocatetomap(NewCDS,yr);
     
     ymapcoverage=ymapcoverage | yield>0;
     amapcoverage=ymapcoverage | area>0;
 end
 
+
+nsg(ymapcoverage,'title',['Yield coverage ' int2str(yr) ' REV ' Revstr],'filename','on')
+
+nsg(amapcoverage,'title',['Area coverage ' int2str(yr) ' REV ' Revstr],'filename','on')
+end
+
+
+
 cd(pwd)
-
-nsg(ymapcoverage)
-
-nsg(amapcoverage)
